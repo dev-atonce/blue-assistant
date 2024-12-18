ee978843db560e6eb2301bc6d8b938953635a6c5
HOCKIIE <spw.kgs@gmail.com>
2024-12-18 16:34:11 +0700

    update

---
+++ client/src/app/[lng]/page.tsx
@@ -0,0 +1,41 @@
+import CoverSwiper from "@/components/website/layout/CoverSwiper";
+
+import Blog from "@/components/website/layout/Blog";
+import Image from "next/image";
+
+const fetchBlog = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACK_END_URL}/api/v1/blog/limit/4`,
+    {
+      cache: "no-store",
+    }
+  );
+  const data = await res.json();
+  return data;
+};
+
+const fetchBanner = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACK_END_URL}/api/v1/banner`,
+    {
+      cache: "no-store",
+    }
+  );
+  const data = await res.json();
+  return data;
+};
+
+export default async function Home() {
+  // const blogs = await fetchBlog();
+  // const banner = await fetchBanner();
+
+  return (
+    <>
+      <div style={{height:'80vh', overflow:'hidden',zIndex:0}}>
+        <Image src="/img/Component 44455.png" width={4838} height={2198} quality={100} alt="Blue Assistance" />
+      </div>
+      <div className="container xl:px-4 2xl:px-4 px-4 mx-auto">
+      </div>
+    </>
+  );
+}
