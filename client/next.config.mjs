e132a2d900aef8e3f910582342a13622985fd524
Nachod Sanguansataya <110734461+Nachodsang@users.noreply.github.com>
2025-01-23 00:01:49 +0700

    config.js

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,25 +4,25 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "http",
-            hostname: "127.0.0.1",
-            port: "3001",
-          },
-          {
-            protocol: "http",
-            hostname: "192.168.0.199",
-            port: "3001",
-          },
-          {
-            protocol: "https",
-            hostname: "api-bluemed.oncewebdesign.com",
-            port: "",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "3001",
       },
+      {
+        protocol: "http",
+        hostname: "192.168.0.199",
+        port: "3001",
+      },
+      {
+        protocol: "https",
+        hostname: "api-blue.oncewebdesign.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
